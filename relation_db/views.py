a6329155576e338eaa4cf42422d7e00afc97cf73
ВÐakramtursunjanov0-stack <akramtursunjanov0@gmail.com>
2026-02-07 14:25:28 +0600

    commit

--- relation_db/views.py
+++ relation_db/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Person
 
-# Create your views here.
+
+def person_list(request):
+    persons = Person.objects.all()
+    return render(request, 'persons.html', {'persons': persons})
